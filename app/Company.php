49221e51d5931da77e137c7d8ea6cba10c1d23d8
dev.umigo <dev.umigo@gmail.com>
2019-09-02 09:30:01 +0100

    added relationship between employee and company

--- app/Company.php
+++ app/Company.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    public function employee()
+    {
+    	$this->hasMany('App/Employee');
+    }
 }
--- database/migrations/2019_09_02_080646_update_employees_table.php
+++ database/migrations/2019_09_02_080646_update_employees_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateCompaniesTable extends Migration
+class UpdateEmployeesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class UpdateCompaniesTable extends Migration
      */
     public function up()
     {
-        Schema::table('companies', function (Blueprint $table) {
-            $table->string('logo')->nullable();
+        Schema::table('employees', function (Blueprint $table) {
+            $table->integer('company_id')->nullable();
         });
     }
 
@@ -25,8 +25,8 @@ class UpdateCompaniesTable extends Migration
      */
     public function down()
     {
-        Schema::table('companies', function (Blueprint $table) {
-            $table->dropColumn('logo');
+        Schema::table('employees', function (Blueprint $table) {
+            $table->dropColumn('company_id');
         });
     }
 }
